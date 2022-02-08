6b6b55756b1cdbb1253689f2382864c8bab09624
annesaa <47871561+annesaa@users.noreply.github.com>
2022-02-07 21:40:56 -0300

    Add files via upload

---
+++ Projeto_Consultas.py
@@ -0,0 +1,386 @@
+from datetime import datetime
+dados = dict()
+dados['Nome Completo'] = str(input('\nNome Completo:'))
+dados['Area de atuação'] = str(input('\nArea de atuação:'))
+dados['Digite seu crm'] = str(input('\nDigite seu crm:'))
+
+
+print('\nBom Dia! Suas consultas agendadas deste mês é : ')
+print('-----------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente: Vanessa da Mata',2,'\nData: 07/12/2020',
+              3,'\nHorario: 10:00',4,'\nRG: 51.514.234.1',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente: Felix Santos',2,'\nData: 07/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 32.154.244.5',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente: Marcia Abreu',2,'\nData: 08/12/2020',
+              3,'\nHorario: 09:00',4,'\nRG: 85.595.655.8',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente: Maria Vitoria C.',2,'\nData: 08/12/2020',
+              3,'\nHorario: 11:30',4,'\nRG: 21.254.789.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente: Maria Vanilde',2,'\nData: 09/12/2020',
+              3,'\nHorario: 08:00',4,'\nRG: 52.487.569.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Vick Sant ',2,'\nData: 09/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 51.514.231.7',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Pedro Silva ',2,'\nData: 10/12/2020',
+              3,'\nHorario: 10:00',4,'\nRG: 45.251.352.2',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Marcelo Analdo ',2,'\nData: 10/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 54.584.625.3',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Mariluce Oliveira ',2,'\nData: 11/12/2020',
+              3,'\nHorario: 10:30',4,'\nRG: 54.875.423.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Vick Sant ',2,'\nData: 11/12/2020',
+              3,'\nHorario: 12:00',4,'\nRG: 54.875.423.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Mirna Fernandes ',2,'\nData: 14/12/2020',
+              3,'\nHorario: 10:30',4,'\nRG: 98.547.589.2',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Meirele Arabella ',2,'\nData: 14/12/2020',
+              3,'\nHorario: 13:30',4,'\nRG: 45.654.844.4',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Mirna Fernandes ',2,'\nData: 15/12/2020',
+              3,'\nHorario: 10:30',4,'\nRG: 87.548.963.2',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Jessica Fernandes P.',2,'\nData: 15/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 74.456.987.7',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Renato Mouth ',2,'\nData: 16/12/2020',
+              3,'\nHorario: 08:30',4,'\nRG: 65.654.258.5',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente: Clara Neves ',2,'\nData: 16/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 65.874.123.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Pedrina Alvarenga ',2,'\nData: 17/12/2020',
+              3,'\nHorario: 08:30',4,'\nRG: 36.985.214.1',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Julia Souza ',2,'\nData: 17/12/2020',
+              3,'\nHorario: 12:30',4,'\nRG: 74.125.589.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Grazi Bias ',2,'\nData: 18/12/2020',
+              3,'\nHorario: 10:00',4,'\nRG: 87.489.651.3',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Aline Andrade ',2,'\nData: 18/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 698.874.522.3',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Alex Oliveira',2,'\nData: 21/12/2020',
+              3,'\nHorario: 10:30',4,'\nRG: 54.885.566.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Tiago rodrigues',2,'\nData: 21/12/2020',
+              3,'\nHorario: 14:00',4,'\nRG: 669.332.221.2',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+for tabel in [1,'\nNome da Paciente:Caik Moraes',2,'\nData: 22/12/2020',
+              3,'\nHorario: 11:00',4,'\nRG: 55.663.332.2',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+for tabel in [1,'\nNome da Paciente:Tatiane Pinto',2,'\nData: 23/12/2020',
+              3,'\nHorario: 13:00',4,'\nRG: 884.554.225.6',5]:
+    if tabel==1:
+        continue
+    if tabel==2:
+        continue
+    if tabel==3:
+        continue
+    if tabel==4:
+        continue
+    if tabel==5:
+        break
+    print(tabel)
+
+print('---------------------------------------------------------------------')
+
+
+
+  
+    
+   
+
